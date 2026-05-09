380aa097ce99177d0acb98905112b14322079e53
shining <safarovjavlonbek74@gmail.com>
2026-05-09 13:26:19 +0300

    NeonAudio

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="neonapi",
+    name="NeonAPI",
     version="1.0.0",
     description="NeonAPI - API interaction and management library.",
     author="Neon Team",
